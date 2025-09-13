27a558ef76188ae0b041cb365f9dfa5444cb6b12
root <root@DESKTOP-8969P1M.localdomain>
2025-09-12 21:53:51 -0700

    Functions mostly done, need to add error cases, main needs to be finished

--- contacts.py
+++ contacts.py
@@ -2,11 +2,22 @@
 # Date: 9/11/25
 # Purpose: This file is supposed to hold the functions that will be used in main.py
 
+def print_list(contacts):
+    """Contact list printing"""
+    print("================== CONTACT LIST ==================")
+    print("Index  First Name           Last Name")
+    print("====== ==================== ====================")
+    for i in range(len(contacts)):
+        print(f'{str(i):8}{contacts[i][0]:22}{contacts[i][1]:22}')
+
 def add_contact(contacts, /, *, first, last):
     """Adds names to the contact list"""
     contacts.append([first, last])
 
 def modify_contact(contacts, /, *, nfirst, nlast, index):
+    """Modifying contact information in system"""
+    n = input("Enter the index number: ")
+    n_int = int(n)
     if -1 <= index < len(contacts):
         contacts[index] = [nfirst, nlast]
         return True
@@ -14,6 +25,7 @@ def modify_contact(contacts, /, *, nfirst, nlast, index):
         return False
     
 def delete_contact(contacts, /, *, index):
+    """Deletes contacts in list"""
     if -1 <= index < len(contacts):
         contacts.pop(contacts[index])
         return True
@@ -21,6 +33,7 @@ def delete_contact(contacts, /, *, index):
         return False
     
 def sort_contacts(contacts, /, *, column):
+    """Sorts names by first and last names"""
     if column == 0:
         contacts.sort(key = lambda name: name.split()[0])
     elif column == 1:
--- main.py
+++ main.py
@@ -5,3 +5,35 @@
 from contacts import *
 
 contacts = None
+choices = ["1", "2", "3", "4", "5", "6", "7"]
+
+while True:
+    print("       *** EMPLOYEE CONTACT MAIN MENU      ")
+    print()
+    print("1. Print list")
+    print("2. Add contact")
+    print("3. Modify contact")
+    print("4. Delete contact")
+    print("5. Sort list by first name")
+    print("6. Sort list by last name")
+    print("7. Exit the program")
+    print()
+    prompt = input("Enter menu choice: ")
+    if prompt == "1":
+        print_list(contacts)
+    elif prompt == "2":
+        add_contact(contacts)
+    elif prompt == "3":
+        modify_contact(contacts)
+    elif prompt == "4":
+        delete_contact(contacts)
+    elif prompt == "5":
+        # ask alton
+    elif prompt == "6":
+        # should be same as 5
+    elif prompt == "7":
+        break
+    if prompt not in choices:
+        print("You've chosen an invalid option try again")
+        continue
+    
